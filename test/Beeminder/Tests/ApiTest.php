3c5bd12a2eadcf8c78d6545d9f09d9306149d48d
= <=>
2015-02-16 11:19:08 +0000

    Add Tests to Beeminder_Tests_ApiTest
    Tests for the proxy methods.
    Coverage 100%

--- test/Beeminder/Tests/ApiTest.php
+++ test/Beeminder/Tests/ApiTest.php
@@ -1,10 +1,40 @@
 <?php
-require_once __DIR__ . '/ApiTestCase.php';
 
-class Beeminder_Tests_ApiTest extends Beeminder_Tests_ApiTestCase
+class Beeminder_Tests_ApiTest extends PHPUnit_Framework_TestCase
 {
-    #TODO: write these tests.
-    public function testSomething()
+    public function testProxyMethods()
     {
+       foreach( array( 'get','put','post','delete') as $method ) {
+            $api = $this->_getMockApiWithExpectation( $method );
+            $api->$method();
+       }
+    }
+
+    protected function _getMockApiWithExpectation( $method )
+    {
+        $driver = $this->getMockBuilder( 'Beeminder_HttpDriver' )->getMock();
+        $api = new Beeminder_Api_Double( $driver );
+
+        $driver->expects($this->once())->method($method);
+        return $api;
+    }
+}
+
+class Beeminder_Api_Double extends Beeminder_Api
+{
+    public function get() {
+        return parent::get( 'path', array(), array() );
+    }
+
+    public function put() {
+        return parent::put( 'path', array(), array() );
+    }
+
+    public function delete() {
+        return parent::delete( 'path', array(), array() );
+    }
+
+    public function post() {
+        return parent::post( 'path', array(), array() );
     }
 }
